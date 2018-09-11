ead956edc9190ba7e5773112b309e71901dd3409
Huei-Horng Yo <hiroshi@ghostsinthelab.org>
2018-09-11 15:27:52 +0800

    Adjust output layout
    Signed-off-by: Huei-Horng Yo <hiroshi@ghostsinthelab.org>

--- array30_practice_table.rb
+++ array30_practice_table.rb
@@ -22,9 +22,10 @@ key_mappings = {
 File.open("array30_key_mappings.txt", "w+") do |file|
   array30_table.each do |chr, code|
     if kanji_bank.chars.include?(chr)
-      keys = code.chars.map { |c| key_mappings[c] }.join(' ')
-      puts "#{chr} - #{code} ( #{keys} )"
-      file << "#{chr} - #{code} ( #{keys} )\n"
+      keys = code.chars.map { |c| "#{c}(#{key_mappings[c]})" }.join(' ')
+      output = sprintf("%-2s%s\n", chr, keys)
+      puts output
+      file << output
     end
   end
 end
